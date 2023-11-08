5e4b47753530704bda2d3ef313730c150b52a58f
Chandrakanth.V <chandrakanthvarakala@gmail.com>
2023-11-08 10:07:27 +0530

    Add: Project Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+        {/* Place holder */}
+        <h1 className='tw-text-4xl tw-bg-orange-600 tw-text-white tw-rounded-lg'>HR Recruitment and Applicant Tracking System</h1>
+		<h1 className='text-success-emphasis'>HR Recruitment and Applicant Tracking System</h1>
+    </>
+  )
+}
+
+export default App
---
+++ readme.md
@@ -0,0 +1,3 @@
+# HR Recruitment and Applicant Tracking System
+
+A platform for HR professional's to manage job postings, applications and candidate evaluations
