12e87cfdbdb9f8ee826e6aa9715162a5dec45bf2
chaitanya709 <chaitanyaprasadam345@gamil.com>
2023-11-09 13:04:22 +0530

    Add:jobs model

---
+++ backend/src/models/jobs.model.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const jobSchema =new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    AppiledBy:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    jobName:{
+        type:String,
+        required:true,
+        lowercase: true
+    },
+    jobDescription:{
+        type:String,
+        required:true
+    },
+    companyName:{
+        type:String,
+        required:true
+    },
+    responsibiliut:{
+        type:String,
+        required:true
+    },
+    qualifiactions:{
+        type:String,
+        required:true
+    },
+    loction:{
+        type:String,
+        required:true
+    },
+    jobtype:{
+        enum:['Full Time','Part Time','Internship']
+    },
+    NoofPosts:{
+        type:String,
+        required:true
+    },
+    Salary:{
+        type:String,
+        required:true
+    },
+    isOpen:{
+        type:Boolean,
+        required:true
+    },
+    skills:[{
+        type:String,
+        required:true
+    }],
+    timestamps: true 
+});
+export const Jobs = mongoose.model('Jobs', jobSchema);
