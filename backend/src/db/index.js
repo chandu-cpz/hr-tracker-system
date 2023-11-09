c10d151485fd5cc367066500fdf562661c931f94
Chandrakanth.V <chandrakanthvarakala@gmail.com>
2023-11-09 12:14:19 +0530

    Add: db connection

---
+++ backend/src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+
+const connectDB = async function() {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`);
+        console.log(`MONGODB CONNECTION SUCCESSFUL DB HOST: ${connectionInstance.connection.host}`);
+    }
+    catch(err) {
+        console.log('MONGODB CONNECTION ERROR',err);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;
