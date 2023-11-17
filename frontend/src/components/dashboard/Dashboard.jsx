596112eb7a68e11417bb166544260e9aecf01607
Chandrakanth.V <chandrakanthvarakala@gmail.com>
2023-11-17 13:23:52 +0530

    Fix: sidebar from html to react navlink react router dom

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -4,10 +4,10 @@ import { Card } from "./Card";
 
 export function Dashboard() {
     const username = "chandu";
-    const data = [
-        { name: "John Doe", email: "john@email.com", phone: "123-456-7890" },
-        { name: "Jane Doe", email: "jane@email.com", phone: "987-654-3210" },
-    ];
+    // const data = [
+    //     { name: "John Doe", email: "john@email.com", phone: "123-456-7890" },
+    //     { name: "Jane Doe", email: "jane@email.com", phone: "987-654-3210" },
+    // ];
     return (
         <>
             <div className="tw-flex">
@@ -16,11 +16,11 @@ export function Dashboard() {
                 <div className="tw-w-full tw-p-4">
                     <div className="tw-mb-8 tw-flex tw-flex-wrap tw-items-center tw-justify-between tw-gap-5">
                         <div>
-                            <h1 className="tw-text-gray-800 tw-text-4xl tw-font-bold">
+                            <h1 className="tw-text-gray-800 tw-mb-5 tw-text-4xl">
                                 Welcome back,
                             </h1>
-                            <h2 className="tw-text-gray-600 tw-text-2xl tw-font-light">
-                                @{username}
+                            <h2 className="tw-text-gray-600 tw-text-5xl tw-font-extrabold">
+                                {username.toUpperCase()}
                             </h2>
                         </div>
                         <Card title="Jobs Open" icon={BsBriefcase} value={12} />
