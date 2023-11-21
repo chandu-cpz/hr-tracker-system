c40bf230740650640a149f45e81ae20c652ceac9
Chandrakanth.V <chandrakanthvarakala@gmail.com>
2023-11-21 16:26:25 +0530

    Fix: image preview and file upload ;

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -3,6 +3,7 @@ import { signUpUser } from "../redux/slice/userSlice";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { MdCameraAlt } from "react-icons/md";
+import uploadFile from "../utils/uploadFile";
 
 export function Signup() {
     const dispatch = useDispatch();
@@ -35,29 +36,20 @@ export function Signup() {
         }));
     };
 
-    // Handle file input change
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        setUserData((prevData) => ({
-            ...prevData,
-            companyImage: file,
-        }));
-    };
-
     const [preview, setPreview] = useState(userData.companyImage);
     const handleImageChange = async (e) => {
         setPreview(URL.createObjectURL(e.target.files[0]));
-        setProfile({
-            ...company,
-            image: e.target.files[0],
+        setUserData({
+            ...userData,
+            companyImage: e.target.files[0],
         });
 
         console.log("We are uploading image");
 
         try {
             const image = await uploadFile(e.target.files[0], "company");
-            setProfile({
-                ...company,
+            setUserData({
+                ...userData,
                 companyImage: image.secure_url,
             });
         } catch (err) {
@@ -220,17 +212,19 @@ export function Signup() {
 
                             <div className="tw-mb-4">
                                 <label className="tw-text-gray-700 tw-mb-2 tw-block tw-text-sm tw-font-bold">
+                                    Company Image:
                                     <MdCameraAlt
                                         size={32}
                                         className="tw-text-gray-500 tw-ml-4 tw-mt-16"
                                     />
-                                    Company Image:
                                 </label>
-                                <img
-                                    src={preview}
-                                    alt="Profile"
-                                    className="tw-h-48 tw-w-48 tw-rounded-full tw-border-4 tw-border-orange-500 tw-object-cover"
-                                />
+                                {userData.companyImage && (
+                                    <img
+                                        src={preview}
+                                        alt="Profile"
+                                        className="tw-m-5 tw-h-48 tw-w-48 tw-rounded-full tw-border-4 tw-border-orange-500 tw-object-cover"
+                                    />
+                                )}
                                 <input
                                     type="file"
                                     id="companyImage"
