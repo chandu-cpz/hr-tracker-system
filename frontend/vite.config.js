e08d5b7268d4785fb45b9f5b6faccbaf2ffb7c26
Chandrakanth.V <chandrakanthvarakala@gmail.com>
2023-11-23 01:00:57 +0530

    Fix: origin issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         proxy: {
             "/api": {
                 target: "https://hr-tracker-system.onrender.com",
-                changeOrigin: true,
             },
         },
     },
