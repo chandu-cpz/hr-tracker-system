9d46ce3031966e9467769c167ff64a3c32a8a617
Chandrakanth.V <chandrakanthvarakala@gmail.com>
2023-11-23 00:18:19 +0530

    Add: change proxy origin to render deployement link:

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                target: "https://hr-tracker-system.onrender.com",
                 changeOrigin: true,
             },
         },
