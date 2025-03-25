2dee6f03d00d54f1f1beedac0c59f630d5a67a2b
Chandrakanth V <chandrakanthvarakala@gmail.com>
2025-03-25 20:21:16 +0530

    Fix: make sure that process doesn't get killed

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Change to the recSys directory and run the Python service in the background,
-# redirecting its output to a log file.
-(cd ../recSys/src && nohup python3 main.py > recSys.log 2>&1 &)
 
-# Start the Node.js backend service
-npm run start
+nohup npm run start > ../../node.log 2>&1 &
+
+cd ../recSys/src/ 
+python main.py
